baadacf23abc0b994df06ed2ed59e89ebdd10b09
Praveen Bisht <prvnbist@gmail.com>
2020-01-07 13:38:23 +0530

    feat: icons

---
+++ example/src/assets/icons/index.js
@@ -0,0 +1,7 @@
+import ClearIcon from "./ClearIcon";
+import SearchIcon from "./SearchIcon";
+import ArrowDownIcon from "./ArrowDownIcon";
+import ArrowUpIcon from "./ArrowUpIcon";
+import PlusIcon from "./PlusIcon";
+
+export { ClearIcon, SearchIcon, ArrowDownIcon, ArrowUpIcon, PlusIcon };
