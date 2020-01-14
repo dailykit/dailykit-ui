7c8fb989a969cf3c3b819685ac6dde8414f621bc
Praveen Bisht <prvnbist@gmail.com>
2020-01-14 16:49:30 +0530

    fix: tunnel crashing on single panel

--- src/components/TunnelVision/Tunnel/index.jsx
+++ src/components/TunnelVision/Tunnel/index.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React from 'react'
 
 const Tunnel = ({ children: panels }) => {
-  return (
-    <div>
-      {panels.map(panel => panel.props.visibility !== "hidden" && panel)}
-    </div>
-  );
-};
+   return (
+      <div>
+         {Array.isArray(panels) &&
+            panels.map(panel => panel.props.visibility !== 'hidden' && panel)}
+         {panels.hasOwnProperty('props') &&
+            panels.props.visibility !== 'hidden' &&
+            panels}
+      </div>
+   )
+}
 
-export default Tunnel;
+export default Tunnel
