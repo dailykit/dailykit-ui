9d39c3ad180b0d31830a777b6f9412f5fce9bbbe
Praveen Bisht <prvnbist@gmail.com>
2020-02-21 16:22:52 +0530

    styles: decreased input value's weight

--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -1,101 +1,107 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components'
 
 export const Field = styled.div(
-  ({ hasValue }) => css`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    position: relative;
-    label {
-      position: absolute;
-      color: #888d9d;
-      font-size: 14px;
-      top: ${hasValue.length > 0 ? "-8px" : "11px"};
-    }
-    span[data-type="error"] {
-      color: #dc4b4b;
-      padding-top: 4px;
-      font-size: 14px;
-      font-style: italic;
-    }
-
-    input[type="text"],
-    input[type="password"],
-    input[type="email"],
-    textarea {
-      width: inherit;
-      font-size: 16px;
-      border: none;
-      font-weight: 600;
-      height: 40px;
-      border-bottom: 1px solid rgba(0, 0, 0, 0.2);
-      &:focus {
-        outline: none;
-        border-bottom: 1px solid rgba(0, 0, 0, 0.5);
-        ~ label {
-          top: -8px;
-        }
+   ({ hasValue }) => css`
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      position: relative;
+      label {
+         position: absolute;
+         color: #888d9d;
+         font-size: 14px;
+         top: ${hasValue.length > 0 ? '-8px' : '11px'};
       }
-    }
-    textarea {
-      height: unset;
-      padding-top: 10px;
-    }
-  `
-);
+      span[data-type='error'] {
+         color: #dc4b4b;
+         padding-top: 4px;
+         font-size: 14px;
+         font-style: italic;
+      }
+
+      input[type='text'],
+      input[type='password'],
+      input[type='email'],
+      textarea {
+         width: inherit;
+         font-size: 16px;
+         border: none;
+         font-weight: 400;
+         height: 40px;
+         border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+         &:focus {
+            outline: none;
+            border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+            ~ label {
+               top: -8px;
+            }
+         }
+         &::placeholder {
+            font-weight: 400;
+         }
+      }
+      textarea {
+         height: unset;
+         padding-top: 10px;
+      }
+   `
+)
 
 export const RadioButton = styled.span`
-    padding: 7px 12px;
-    font-weight: 500;
-    font-size: 12px;
-    line-height: 14px;
-    min-width: 70px;
-    color: ${ props => props.active ? '#FFFFFF' : ' #888D9D' };
-    background : ${ props => props.active ? 'linear-gradient(180deg, #28C1F7 -4.17%, #00A7E1 100%)' : '#F3F3F3' };
+   padding: 7px 12px;
+   font-weight: 500;
+   font-size: 12px;
+   line-height: 14px;
+   min-width: 70px;
+   color: ${props => (props.active ? '#FFFFFF' : ' #888D9D')};
+   background: ${props =>
+      props.active
+         ? 'linear-gradient(180deg, #28C1F7 -4.17%, #00A7E1 100%)'
+         : '#F3F3F3'};
 
-    &:not(:last-child) {
+   &:not(:last-child) {
       margin-right: 2px;
-    }
+   }
 
-    &:hover {
+   &:hover {
       cursor: pointer;
-    }
+   }
 `
 
 export const DropDown = styled.div`
-    display: flex;
-    flex-direction: column;
+   display: flex;
+   flex-direction: column;
 
-    > div:nth-child(1){
+   > div:nth-child(1) {
       display: flex;
       font-style: normal;
       font-weight: 500;
       font-size: 14px;
       line-height: 16px;
-      color: #555B6E;
+      color: #555b6e;
       padding: 10px;
       min-width: 70px;
 
-      span:nth-child(1){
-        margin-right: 10px;
+      span:nth-child(1) {
+         margin-right: 10px;
       }
-    }
+   }
 
-    div:nth-child(2) > div{
+   div:nth-child(2) > div {
       padding: 10px;
-    }
+   }
 
-    &:hover {
+   &:hover {
       cursor: pointer;
-    }
+   }
 `
 
 export const TextAndSelectStyle = styled.div`
-    display: flex;
-    align-items: baseline;
+   display: flex;
+   align-items: baseline;
 
-    div:nth-child(1) {
+   div:nth-child(1) {
       margin-right: 10px;
-    }
+   }
 `
