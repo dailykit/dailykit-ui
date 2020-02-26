fe719d9dc4f122fae94fe515d7e32d810205114d
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-02-26 20:19:43 +0530

    fix: #26 onClick on ButtonTile

--- src/components/Tiles/ButtonTile.jsx
+++ src/components/Tiles/ButtonTile.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import { PlusIcon } from "../../assets/icons";
 import { StyledButtonTile } from './styles';
 
-const ButtonTile = ({ type, size, text, noIcon }) => (
-  <StyledButtonTile type={type} size={size}>
+const ButtonTile = ({ type, size, text, noIcon, ...props }) => (
+  <StyledButtonTile type={type} size={size} { ...props }>
     {!noIcon && <PlusIcon size={type === "primary" ? 32 : 20} color="#555b6e"/>}
     <div> {text} </div>
   </StyledButtonTile>
