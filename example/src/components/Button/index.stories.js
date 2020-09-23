6de738e804c64beb2b98d630e73566c0bd7bdbbf
Praveen Bisht <prvnbist@gmail.com>
2020-09-23 14:24:17 +0530

    feat(buttons): small variant

--- example/src/components/Button/index.stories.js
+++ example/src/components/Button/index.stories.js
@@ -13,37 +13,90 @@ import {
 
 storiesOf('Button', module)
    .add('Text Button', () => (
-      <ButtonGroup align='left'>
-         <TextButton type='solid'>Solid Button</TextButton>
-         <TextButton type='outline'>Outline Button</TextButton>
-         <TextButton type='ghost'>Ghost Button</TextButton>
-      </ButtonGroup>
+      <>
+         <ButtonGroup align='left'>
+            <TextButton type='solid' size='sm'>
+               Solid Button
+            </TextButton>
+            <TextButton type='solid'>Solid Button</TextButton>
+         </ButtonGroup>
+         <br />
+         <ButtonGroup align='left'>
+            <TextButton type='outline' size='sm'>
+               Outline Button
+            </TextButton>
+            <TextButton type='outline'>Outline Button</TextButton>
+         </ButtonGroup>
+         <br />
+         <ButtonGroup align='left'>
+            <TextButton type='ghost' size='sm'>
+               Ghost Button
+            </TextButton>
+            <TextButton type='ghost'>Ghost Button</TextButton>
+         </ButtonGroup>
+      </>
    ))
    .add('Icon Button', () => (
-      <ButtonGroup align='left'>
-         <IconButton type='solid'>
-            <PlusIcon />
-         </IconButton>
-         <IconButton type='outline'>
-            <SearchIcon />
-         </IconButton>
-         <IconButton type='ghost'>
-            <SearchIcon />
-         </IconButton>
-      </ButtonGroup>
+      <>
+         <ButtonGroup align='left'>
+            <IconButton type='solid' size='sm'>
+               <PlusIcon />
+            </IconButton>
+            <IconButton type='solid'>
+               <PlusIcon />
+            </IconButton>
+         </ButtonGroup>
+         <br />
+         <ButtonGroup align='left'>
+            <IconButton type='outline' size='sm'>
+               <SearchIcon />
+            </IconButton>
+            <IconButton type='outline'>
+               <SearchIcon />
+            </IconButton>
+         </ButtonGroup>
+         <br />
+         <ButtonGroup align='left'>
+            <IconButton type='ghost' size='sm'>
+               <SearchIcon />
+            </IconButton>
+            <IconButton type='ghost'>
+               <SearchIcon />
+            </IconButton>
+         </ButtonGroup>
+      </>
    ))
    .add('Combo Button', () => (
-      <ButtonGroup align='left'>
-         <ComboButton type='solid'>
-            <SearchIcon /> Search
-         </ComboButton>
-         <ComboButton type='outline'>
-            Add Ingredient
-            <PlusIcon />
-         </ComboButton>
-         <ComboButton type='ghost'>
-            <ClearIcon />
-            Clear Filters
-         </ComboButton>
-      </ButtonGroup>
+      <>
+         <ButtonGroup align='left'>
+            <ComboButton type='solid' size='sm'>
+               <SearchIcon /> Search
+            </ComboButton>
+            <ComboButton type='solid'>
+               <SearchIcon /> Search
+            </ComboButton>
+         </ButtonGroup>
+         <br />
+         <ButtonGroup align='left'>
+            <ComboButton type='outline' size='sm'>
+               Add Ingredient
+               <PlusIcon />
+            </ComboButton>
+            <ComboButton type='outline'>
+               Add Ingredient
+               <PlusIcon />
+            </ComboButton>
+         </ButtonGroup>
+         <br />
+         <ButtonGroup align='left'>
+            <ComboButton type='ghost' size='sm'>
+               <ClearIcon />
+               Clear Filters
+            </ComboButton>
+            <ComboButton type='ghost'>
+               <ClearIcon />
+               Clear Filters
+            </ComboButton>
+         </ButtonGroup>
+      </>
    ))
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,149 +1,204 @@
 import styled, { css } from 'styled-components'
 
-export const StyledTextButton = styled.button`
-  height: 40px;
-  padding: 0 16px;
-  font-weight: 400;
-  font-size: 16px;
-  color: #ffffff;
-  cursor: pointer;
-  border-radius: 2px;
-  ${({ type }) =>
-     type === 'outline' &&
-     css`
-        color: #00a7e1;
-        background: transparent;
-        border: 1px solid #00a7e1;
-        &:hover {
-           color: #fff;
-           background: linear-gradient(180deg, #28c1f7 -4.17%, #00a7e1 100%);
-        }
-     `}
-  ${({ type }) =>
-     type === 'solid' &&
-     css`
-        color: #fff;
-        border: none;
-        background: linear-gradient(180deg, #28c1f7 -4.17%, #00a7e1 100%);
-     `}
-  ${({ type }) =>
-     type === 'ghost' &&
-     css`
-        color: #00a7e1;
-        border: none;
-        background: transparent;
-        &:hover {
-           background: #f5f5f5;
-        }
-     `}
+const dimensions = ({ h, p, w, fW, fS }) => css`
+   width: ${w || 'auto'};
+   height: ${h || 'auto'};
+   padding: ${p || 'unset'};
+   font-size: ${fS || 'unset'};
+   font-weight: ${fW || 'unset'};
 `
+
+const sizeTextSelector = size => {
+   switch (size) {
+      case 'sm':
+         return css`
+            ${dimensions({ h: '32px', p: '0 8px', fW: '400', fS: '14px' })}
+         `
+      default:
+         return css`
+            ${dimensions({ h: '40px', p: '0 16px', fW: '400', fS: '16px' })}
+         `
+   }
+}
+
+export const StyledTextButton = styled.button`
+   color: #ffffff;
+   cursor: pointer;
+   border-radius: 2px;
+   ${({ size }) => sizeTextSelector(size)}
+   ${({ type }) =>
+      type === 'outline' &&
+      css`
+         color: #00a7e1;
+         background: transparent;
+         border: 1px solid #00a7e1;
+         &:hover {
+            color: #fff;
+            background: linear-gradient(180deg, #28c1f7 -4.17%, #00a7e1 100%);
+         }
+      `}
+  ${({ type }) =>
+      type === 'solid' &&
+      css`
+         color: #fff;
+         border: none;
+         background: linear-gradient(180deg, #28c1f7 -4.17%, #00a7e1 100%);
+      `}
+  ${({ type }) =>
+      type === 'ghost' &&
+      css`
+         color: #00a7e1;
+         border: none;
+         background: transparent;
+         &:hover {
+            background: #f5f5f5;
+         }
+      `}
+`
+
+const sizeIconSelector = size => {
+   switch (size) {
+      case 'sm':
+         return css`
+            ${dimensions({
+               h: '32px',
+               w: '32px'
+            })}
+         `
+      default:
+         return css`
+            ${dimensions({
+               h: '40px',
+               w: '40px'
+            })}
+         `
+   }
+}
 
 export const StyledIconButton = styled.button`
-  height: 40px;
-  width: 40px;
-  border: none;
-  cursor: pointer;
-  display: flex;
-  border-radius: 2px;
-  align-items: center;
-  justify-content: center;
+   border: none;
+   cursor: pointer;
+   display: flex;
+   border-radius: 2px;
+   align-items: center;
+   justify-content: center;
+   ${({ size }) => sizeIconSelector(size)}
+   ${({ type }) =>
+      type === 'outline' &&
+      css`
+         background: transparent;
+         border: 1px solid #00a7e1;
+         svg {
+            stroke: #00a7e1;
+         }
+         &:hover {
+            background: linear-gradient(180deg, #28c1f7 -4.17%, #00a7e1 100%);
+            svg {
+               stroke: #fff;
+            }
+         }
+      `}
+   ${({ type }) =>
+      type === 'solid' &&
+      css`
+         color: #fff;
+         border: none;
+         background: linear-gradient(180deg, #28c1f7 -4.17%, #00a7e1 100%);
+      `}
   ${({ type }) =>
-     type === 'outline' &&
-     css`
-        background: transparent;
-        border: 1px solid #00a7e1;
-        svg {
-           stroke: #00a7e1;
-        }
-        &:hover {
-           background: linear-gradient(180deg, #28c1f7 -4.17%, #00a7e1 100%);
-           svg {
-              stroke: #fff;
-           }
-        }
-     `}
-  ${({ type }) =>
-     type === 'solid' &&
-     css`
-        color: #fff;
-        border: none;
-        background: linear-gradient(180deg, #28c1f7 -4.17%, #00a7e1 100%);
-     `}
-  ${({ type }) =>
-     type === 'ghost' &&
-     css`
-        border: none;
-        background: transparent;
-        &:hover {
-           background: #f5f5f5;
-        }
-     `}
+      type === 'ghost' &&
+      css`
+         border: none;
+         background: transparent;
+         &:hover {
+            background: #f5f5f5;
+         }
+      `}
 `
 
-export const StyledComboButton = styled.button`
-  height: 40px;
-  display: flex;
-  font-size: 16px;
-  cursor: pointer;
-  font-weight: 400;
-  align-items: center;
-  border-radius: 2px;
-  span {
-    width: 40px;
-    height: 40px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  ${({ position }) =>
-     position === 'right'
-        ? css`
-             padding: 0 0 0 14px;
-          `
-        : css`
-             padding: 0 28px 0 0;
-          `}
+const sizeComboSelector = size => {
+   switch (size) {
+      case 'sm':
+         return css`
+            ${dimensions({
+               h: '32px',
+               fW: '400',
+               fS: '14px'
+            })}
+         `
+      default:
+         return css`
+            ${dimensions({
+               h: '40px',
+               fW: '400',
+               fS: '16px'
+            })}
+         `
+   }
+}
 
+export const StyledComboButton = styled.button`
+   display: flex;
+   cursor: pointer;
+   border-radius: 2px;
+   align-items: center;
+   ${({ size }) => sizeComboSelector(size)}
+   span {
+      display: flex;
+      height: inherit;
+      align-items: center;
+      justify-content: center;
+      width: ${({ size }) => (size === 'sm' ? '32px' : '40px')};
+   }
+   ${({ position, size }) =>
+      position === 'right'
+         ? css`
+              padding: 0 0 0 ${size === 'sm' ? '8px' : '14px'};
+           `
+         : css`
+              padding: 0 ${size === 'sm' ? '8px' : '14px'} 0 0;
+           `}
+
+   ${({ type }) =>
+      type === 'outline' &&
+      css`
+         color: #00a7e1;
+         background: transparent;
+         border: 1px solid #00a7e1;
+         svg {
+            stroke: #00a7e1;
+         }
+         &:hover {
+            color: #fff;
+            background: linear-gradient(180deg, #28c1f7 -4.17%, #00a7e1 100%);
+            svg {
+               stroke: #fff;
+            }
+         }
+      `}
   ${({ type }) =>
-     type === 'outline' &&
-     css`
-        color: #00a7e1;
-        background: transparent;
-        border: 1px solid #00a7e1;
-        svg {
-           stroke: #00a7e1;
-        }
-        &:hover {
-           color: #fff;
-           background: linear-gradient(180deg, #28c1f7 -4.17%, #00a7e1 100%);
-           svg {
-              stroke: #fff;
-           }
-        }
-     `}
+      type === 'solid' &&
+      css`
+         color: #fff;
+         border: none;
+         background: linear-gradient(180deg, #28c1f7 -4.17%, #00a7e1 100%);
+         svg {
+            stroke: #fff;
+         }
+      `}
   ${({ type }) =>
-     type === 'solid' &&
-     css`
-        color: #fff;
-        border: none;
-        background: linear-gradient(180deg, #28c1f7 -4.17%, #00a7e1 100%);
-        svg {
-           stroke: #fff;
-        }
-     `}
-  ${({ type }) =>
-     type === 'ghost' &&
-     css`
-        color: #00a7e1;
-        border: none;
-        background: transparent;
-        svg {
-           stroke: #00a7e1;
-        }
-        &:hover {
-           background: #f5f5f5;
-        }
-     `}
+      type === 'ghost' &&
+      css`
+         color: #00a7e1;
+         border: none;
+         background: transparent;
+         svg {
+            stroke: #00a7e1;
+         }
+         &:hover {
+            background: #f5f5f5;
+         }
+      `}
 `
 
 export const StyledButtonGroup = styled.div(
